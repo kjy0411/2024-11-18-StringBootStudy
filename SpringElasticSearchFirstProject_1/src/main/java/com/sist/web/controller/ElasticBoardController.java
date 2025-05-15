3bc06c3113c3992f673dcd1ab035438cc3bb2b88
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2025-05-15 16:49:17 +0900

    강의 : Spring-Boot ElasticSearch 게시글 등록

--- SpringElasticSearchFirstProject_1/src/main/java/com/sist/web/controller/ElasticBoardController.java
+++ SpringElasticSearchFirstProject_1/src/main/java/com/sist/web/controller/ElasticBoardController.java
@@ -8,8 +8,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import com.sist.web.vo.*;
 import com.sist.web.dao.*;
@@ -30,15 +32,24 @@ public class ElasticBoardController {
     	Page<ElasticBoard> pList=bDao.findAll(pg);
     	// Page => List로 변환 
     	List<ElasticBoard> list=new ArrayList<ElasticBoard>();
-    	if(pList!=null && pList.hasContent())
-    	{
+    	if(pList!=null && pList.hasContent()){
     		list=pList.getContent(); // Page => List변환 
     	}
-    	
-    	ElasticBoard eb=list.get(0);
+    	for(ElasticBoard eb:list) {
+    		String s=eb.getRegdate();
+    		int a=s.indexOf("T");
+    		if(a>0) {
+    			s=s.substring(0,s.indexOf("T")); // %Y-%m-%d TO_CHAR
+    			eb.setRegdate(s);
+    		}
+    	}
+    	/*
+    	int idx=list.size()-1;
+    	ElasticBoard eb=list.get(idx);
     	String s=eb.getRegdate();
     	s=s.substring(0,s.indexOf("T")); // %Y-%m-%d TO_CHAR
-    	list.get(0).setRegdate(s);
+    	list.get(idx).setRegdate(s);
+    	*/
     	// 총페이지 구하기 
     	int count=(int)bDao.count();
     	int totalpage=(int)(Math.ceil(count/10.0));
@@ -56,5 +67,36 @@ public class ElasticBoardController {
         model.addAttribute("list", list);
         
     	return "eboard/list";
+    }
+    @GetMapping("/eboard/insert")
+    public String eboard_insert() {
+    	return "eboard/insert";
+    }
+    @PostMapping("/eboard/insert_ok")
+    public String eboard_insert_ok(ElasticBoard vo) {
+    	vo.setHit(0);
+    	vo.setId(idMaxData());
+    	vo.setRegdate(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+    	bDao.save(vo);
+    	return "redirect:/eboard/list";
+    }
+    public int idMaxData() {
+    	int max=0;
+    	try {
+        	int rowSize=10;
+        	int start=0;
+        	Pageable pg=PageRequest.of(start, rowSize,Sort.by(Sort.Direction.DESC,"id"));
+        	//   전체 데이터 읽기 => findAll => SELECT ~~ 
+        	Page<ElasticBoard> pList=bDao.findAll(pg);
+        	// Page => List로 변환 
+        	List<ElasticBoard> list=new ArrayList<ElasticBoard>();
+        	if(pList!=null && pList.hasContent()) {
+        		list=pList.getContent(); // Page => List변환
+        		max=list.get(0).getId();
+        	}
+		} catch (Exception e) {
+			max=0;
+		}
+    	return max+1;
     }
 }
